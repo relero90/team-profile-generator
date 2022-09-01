3a46149252e7495315ad4b9eadf3ebd73fe2001c
relero90 <relero90@gmail.com>
2022-08-31 19:13:47 -0600

    added test for getEmail() function

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -13,7 +13,14 @@ describe("Employee class", () => {
   });
 });
 
-//getName()
-//getId()
-//getEmail()
-//getRole()—returns 'Employee'
+describe("getEmail() function", () => {
+  it("returns a string that functions as an HTML mailto: anchor tag", () => {
+    // Arrange & Act
+    const bob = new Employee("Bob", 2, "test@gmail.com");
+    const emailLink = bob.getEmail();
+    // Assert
+    expect(emailLink).toEqual(
+      `<a href="mailto:test@gmail.com">test@gmail.com</a>`
+    );
+  });
+});
